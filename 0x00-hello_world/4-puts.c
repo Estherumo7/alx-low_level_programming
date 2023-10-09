d5f6cb413fbc8779adc0303e2ace8c6bca61d13d
Esther Umo <umoesther70@gmail.com>
2023-10-09 15:41:42 -0700

    sigh

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point of this program
+ *
+ * This program prints text using the puts function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}
